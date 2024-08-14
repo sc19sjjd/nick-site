7881a72e43df5e9da5514becee2b7c1f6da24651
Stuart Dingwell <stuartdingwell@gmail.com>
2024-08-14 17:07:46 -0600

    title section add ellipse to bottom transition

--- components/Index/AboutSection.vue
+++ components/Index/AboutSection.vue
@@ -1,45 +1,43 @@
 <template>
-  <div class="absolute rounded-full bg-background -z-10 top-0 left-0" 
-  style="width: 500px; height:">
-  </div>
+  <WrappersContent class="bg-orange-100 z-10">
+    <div class="w-full pt-24"></div>
 
-  <WrappersContent class="bg-orange-200 w-full min-h-dvh py-20">
-    <h2 class="text-orange-700 text-center text-6xl" style="font-family: 'Archivo Black', sans-serif;">
-      About me
-    </h2>
+    <div class="min-h-dvh flex flex-col text-black justify-between py-20">
+      <h2 class="text-orange-700 text-center text-6xl" style="font-family: 'Archivo Black', sans-serif;">
+        About me
+      </h2>
 
-    <div class="text-black flex items-center h-full">
-      <div>
-        <p class="pb-14 max-w-md">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Mauris euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
-          Sed euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
-          Sed euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euis
-        </p>
-        <p class="max-w-md pb-10">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Mauris euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
-          Sed euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Mauris euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
-        </p>
-        <p class="max-w-md pb-10">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Mauris euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
-          Sed euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Mauris euismod, nisl eget ultricies ultrices, 
-          nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
-        </p>
-      </div>
+      <!-- <div class="text-black flex-col justify-between h-full"> -->
+          <p class="max-w-md">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Mauris euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
+            Sed euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
+            Sed euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euis
+          </p>
+          <p class="max-w-md">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Mauris euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
+            Sed euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Mauris euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
+          </p>
+          <p class="max-w-md">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Mauris euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod. 
+            Sed euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Mauris euismod, nisl eget ultricies ultrices, 
+            nisl nisi aliquam nisl, euismod aliquet nisi nunc euismod.
+          </p>
+      <!-- </div> -->
     </div>
   </WrappersContent>
 </template>
--- components/Index/TitleSection.vue
+++ components/Index/TitleSection.vue
@@ -1,18 +1,34 @@
 <template>
-  <WrappersFullScreen class="flex-col">
-    <!-- <div class="w-full h-80 sm:h-72 lg:h-64"></div> -->
-    <div class="flex justify-center h-dvh items-center xsm:justify-between">
-      <div class="hidden xsm:block"></div>
-      <div class="flex flex-col">
-        <h1 class="text-orange-700 text-6xl xsm:text-7xl sm:text-8xl lg:text-9xl" style="font-family: 'Archivo black', sans-serif;">
-          NICHOLAS <br> CLAY
-        </h1>
-        <div class="xsm:pb-1 sm:pb-2 lg:pb-2"></div>
-        <h3 class="text-xl xsm:text-xl sm:text-2xl lg:text-3xl pl-1 xsm:pl-2 lg:pl-3">
-          3D modelling <br class="hidden xsm:block"> and <br class="hidden xsm:block"> animation
-        </h3>
+  <div class="relative z-10">
+    <WrappersFullScreen class="flex-col">
+      <!-- <div class="w-full h-80 sm:h-72 lg:h-64"></div> -->
+      <div class="flex justify-center h-dvh items-center xsm:justify-between">
+        <div class="hidden xsm:block"></div>
+        <div class="flex flex-col">
+          <h1 class="text-orange-700 text-6xl xsm:text-7xl sm:text-8xl lg:text-9xl" style="font-family: 'Archivo black', sans-serif;">
+            NICHOLAS <br> CLAY
+          </h1>
+          <div class="xsm:pb-1 sm:pb-2 lg:pb-2"></div>
+          <h3 class="text-xl xsm:text-xl sm:text-2xl lg:text-3xl pl-1 xsm:pl-2 lg:pl-3">
+            3D modelling <br class="hidden xsm:block"> and <br class="hidden xsm:block"> animation
+          </h3>
+        </div>
       </div>
+      <!-- <div class="w-full h-80 sm:h-72 lg:h-64"></div> -->
+    </WrappersFullScreen>
+
+    <div class="absolute -z-10 left-0 top-0 w-full h-full flex justify-center items-end">
+      <div class="ellipse bg-background"></div>
     </div>
-    <!-- <div class="w-full h-80 sm:h-72 lg:h-64"></div> -->
-  </WrappersFullScreen>
+  </div>
 </template>
+
+<style scoped>
+  .ellipse {
+    clip-path: ellipse(70% 50% at center);
+    width: 100%;
+    height: 80%;
+    min-width: 1100px;
+    transform: translateY(7rem);
+  }
+</style>
