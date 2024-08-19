d0d06a51d464433b81209c5ce774d6533531fb82
Stuart Dingwell <stuartdingwell@gmail.com>
2024-08-18 22:17:42 -0700

    add case study arrow icon + border change

--- components/Index/Portoflio/CaseStudyCard.vue
+++ components/Index/Portoflio/CaseStudyCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full max-w-md flex flex-col bg-gray-100/5 z-20 rounded-xl border-lightGrey border-0 shadow-none shadow-black">
+  <div class="w-full h-full max-w-md flex flex-col bg-gray-100/5 z-20 rounded-xl border-orange-900 border-0 shadow-none shadow-black">
     <div class="flex flex-col p-0">
       <img class="rounded-xl rounded-bl-none rounded-br-none border-lightGrey border-b-0 w-full z-30" 
       src="/images/Environment tryout 1.png" alt="Case study 1" />
@@ -12,10 +12,12 @@
         </p>
       </div>
     </div>
-    <div class="flex justify-end border-lightGrey border-0 border-t-0">
-      <div class="border-lightGrey border-t-2 border-l-2 rounded-tl-xl px-3 py-1">
-        <div class="text-center">
+    <div class="flex justify-end pt-1">
+      <div class="border-lightGrey border-t border-l rounded-tl-xl px-3 py-1">
+        <div class="text-center flex flex-row">
           Read more
+          <img src="/public/icons/arrow-white.png" alt="arrow" class="pl-1 w-5 h-4" 
+          style="transform: translateY(0.4rem) rotate(-45deg);" />
         </div>
       </div>
     </div>
