b59c8c3b4acfcb132a780bf379c0d2d7b7b9861e
Stuart Dingwell <stuartdingwell@gmail.com>
2024-08-28 16:18:29 +0100

    case study card increase border width, deepen colour

--- components/Index/Portoflio/CaseStudyCard.vue
+++ components/Index/Portoflio/CaseStudyCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full h-full max-w-md flex flex-col bg-gray-100/5 z-20 rounded-xl 
-  border-orange-700 border-0 shadow-none shadow-black hover:shadow-lg hover:shadow-black 
-  hover:border hover:cursor-pointer group transition duration-300">
+  <div class="w-full h-full max-w-md flex flex-col bg-gray-100/5 z-20 rounded-xl box-border 
+  border-orange-800 border-0 shadow-none shadow-black hover:shadow-lg hover:shadow-black 
+  hover:border-2 hover:border-t-orange-700 hover:border-l-orange-700 hover:cursor-pointer group">
     <div class="flex flex-col p-0">
       <img class="rounded-xl rounded-bl-none rounded-br-none border-lightGrey group-hover:border-orange-600 border-b-0 w-full z-30" 
       src="/images/Environment tryout 1.png" alt="Case study 1" />
@@ -15,7 +15,7 @@
       </div>
     </div>
     <div class="flex justify-end  pt-1">
-      <div class="border-lightGrey border-t border-l group-hover:border-orange-600 rounded-tl-xl px-3 py-1">
+      <div class="border-lightGrey border-t-2 border-l-2 group-hover:border-orange-700 rounded-tl-xl px-3 py-1">
         <div class="text-center flex flex-row">
           Read more
           <img src="/public/icons/arrow-white.png" alt="arrow" class="pl-1 w-5 h-4" 
