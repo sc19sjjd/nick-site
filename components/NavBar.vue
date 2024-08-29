97b4ca714ea54882e0e2efe561a25254f684585f
Stuart Dingwell <stuartdingwell@gmail.com>
2024-08-29 17:08:37 +0100

    navbar font bold

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <WrappersFullScreen class="flex justify-between pt-2 z-50
-  underline-offset-2 decoration-2" style="font-family: 'Outfit', sans-serif;">
+  underline-offset-2 decoration-2 font-bold">
     <div class="hover:cursor-pointer hover:text-orange-600
     decoration-2 underline-offset-3" :class="{ 'underline': underline == 'home' }" @click="scrollTo('title')">
       Home
