4d6064c054054675410e0fe8eac32ac21c6fbe86
Stuart Dingwell <stuartdingwell@gmail.com>
2024-09-02 13:48:44 +0100

    contact modal, add linked in and email styling

--- components/ContactModal.vue
+++ components/ContactModal.vue
@@ -1,16 +1,59 @@
 <template>
-  <div id="contact-modal" ref="contactModalEl" class="modal-content w-full hidden h-dvh fixed top-0 left-0 bg-black/50" style="z-index: 1000">
+  <div id="contact-modal" ref="contactModalEl" class="w-full hidden h-dvh fixed top-0 left-0 bg-black/50" style="z-index: 1000">
     <div ref="contactModalOverlayEl" class="w-full h-full flex justify-end">
-    <div ref="contactModalContentEl" class="h-full w-content modal-content bg-background/90">
-      <h1 class="inline-block" >TEST</h1>
+    <div ref="contactModalContentEl" class="h-fit bg-background/90 font-black text-3xl min-w-4/12 rounded-bl-lg">
+      <div class="linked-in px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer">
+        LinkedIn
+        <img src="/public/icons/linkedin.png" alt="linkedin" class="w-8 pl-3 box-content" />
+      </div>
+      <div class="email px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer">
+        nicholasclay@gmail.com
+        <img src="/public/icons/email.png" alt="email" class="w-10 pl-2 box-content" />
+      </div>
+
+      <div class="absolute top-0 right-0 pr-6 pt-6 z-10">
+        <div @click="closeModal" class="cursor-pointer z-30">
+          <img src="/public/icons/cross-white.png" alt="close" class="w-4 box-content" />
+        </div>
+      </div>
     </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .modal-content {
-    transition: all 0.5s ease-in-out;
+  .linked-in::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform-origin: bottom center;
+    transform: scaleY(0);
+    background: #0284c7;
+    z-index: -1;
+    opacity: 0.9;
+    transition: transform 0.4s;
+  }
+
+  .linked-in:hover::after, .email:hover::after {
+    transform: scaleY(1);
+  }
+
+  .email::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform-origin: bottom center;
+    transform: scaleY(0);
+    background: #b91c1c;
+    z-index: -1;
+    opacity: 0.9;
+    transition: transform 0.4s;
   }
 
   .modal-closed {
@@ -42,13 +85,13 @@
   }
 
   @keyframes slide-in {
-    from { transform: translateX(100%); }
-    to { transform: translateX(0); }
+    from { transform: translateY(-100%); }
+    to { transform: translateY(0); }
   }
 
   @keyframes slide-out {
-    from { transform: translateX(0); }
-    to { transform: translateX(100%); }
+    from { transform: translateY(0); }
+    to { transform: translateY(-100%); }
   }
 </style>
 
@@ -71,7 +114,6 @@
     if (!contactModalEl.value) {
       return;
     }
-    // contactModalEl.value!.style.display = ('none');
     contactModalEl.value!.classList.remove('modal-open');
     contactModalEl.value!.classList.add('modal-closed');
   }
