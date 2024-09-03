b0014be74a8a695d1e92b48f4df8cff5eb82ee4a
Stuart Dingwell <stuartdingwell@gmail.com>
2024-09-03 17:12:15 +0100

    contact modal linkedIn and email click events

--- components/ContactModal.vue
+++ components/ContactModal.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="contact-modal" ref="contactModalEl" class="w-full hidden h-dvh fixed top-0 left-0 bg-black/50" style="z-index: 1000">
     <div ref="contactModalOverlayEl" class="w-full h-full flex justify-end">
-    <div ref="contactModalContentEl" class="h-fit bg-background/90 font-black text-3xl min-w-4/12 rounded-bl-lg overflow-hidden">
-      <div class="linked-in px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer">
-        LinkedIn
-        <img src="/public/icons/linkedin.png" alt="linkedin" class="w-8 pl-3 box-content" />
-      </div>
 
-      <div class="email px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer">
-        nicholasclay@gmail.com
-        <img src="/public/icons/email.png" alt="email" class="w-10 pl-2 box-content" />
-      </div>
-
-      <div class="absolute top-0 right-0 pr-6 pt-6 z-10">
-        <div @click="closeModal" class="cursor-pointer z-30 opacity-60 hover:opacity-100 transition duration-300">
-          <img src="/public/icons/cross-white.png" alt="close" class="w-4 box-content" />
+      <div ref="contactModalContentEl" class="h-fit bg-background/90 font-black text-3xl min-w-4/12 rounded-bl-lg relative">
+        <div class="linked-in px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer" @click="() => openLinkedIn()">
+          LinkedIn
+          <img src="/public/icons/linkedin.png" alt="linkedin" class="w-8 pl-3 box-content" />
         </div>
+
+        <div class="email px-6 pt-4 pb-4 flex items-center relative z-10 hover:cursor-pointer rounded-bl-lg overflow-hidden" 
+        @click="() => openEmail()">
+          nicholasclay@gmail.com
+          <img src="/public/icons/email.png" alt="email" class="w-10 pl-2 box-content" />
+        </div>
+
+        <div class="absolute top-0 right-0 pr-6 pt-6 z-10">
+          <div @click="closeModal" class="cursor-pointer z-30 opacity-60 hover:opacity-100 transition duration-300">
+            <img src="/public/icons/cross-white.png" alt="close" class="w-4 box-content" />
+          </div>
+        </div>
+
+        <div ref="emailAlertEl" class="absolute bottom-0 left-0 w-full hidden" style="transform: translateY(3.5rem);">
+          <div class="w-full flex justify-center">
+            <div class="rounded-lg bg-background/90 p-3 w-fit text-center">
+              <p class="text-sm text-white">
+                Email copied to clipboard
+              </p>
+            </div>  
+          </div>
+        </div>
+
       </div>
-    </div>
+
     </div>
   </div>
 </template>
@@ -75,6 +89,11 @@
     animation: slide-out 0.3s ease-in-out;
   }
 
+  .email-copied {
+    animation: fade-in 0.3s ease-in-out, fade-out 0.3s 2s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
   @keyframes fade-in {
     from { opacity: 0; display: none; }
     to { opacity: 1; display: block; }
@@ -101,6 +120,7 @@
 
   const contactModalEl = ref<NullableHTMLElement>(null);
   const contactModalOverlayEl = ref<NullableHTMLElement>(null);
+  const emailAlertEl = ref<NullableHTMLElement>(null);
 
   const openModal = () => {
     if (!contactModalEl.value) {
@@ -127,14 +147,27 @@
     }
   }
 
-  watchEffect(() => {
-    if (!contactModalEl.value) {
+  const openLinkedIn = () => {
+    window.open('https://www.linkedin.com/in/nicholas-clay-uk/', '_blank')?.focus();
+  };
+
+  const openEmail = () => {
+    // window.location.href = 'mailto:nicholasclay@gmail.com';
+    navigator.clipboard.writeText('nicholasclay@gmail.com');
+    alertEmailCopied();
+  };
+
+  const alertEmailCopied = () => {
+    if (!emailAlertEl.value) {
       return;
     }
-    else if (contactModalEl.value!.classList.contains('modal-open')) {
-      contactModalEl.value!.classList.remove('hidden');
-    }
-  });
+    emailAlertEl.value!.classList.remove('hidden');
+    emailAlertEl.value!.classList.add('email-copied');
+    setTimeout(() => {
+      emailAlertEl.value!.classList.remove('email-copied');
+      emailAlertEl.value!.classList.add('hidden');
+    }, 2300);
+  };
 
   onMounted(() => {
     window.addEventListener('click', onWindowClick);
