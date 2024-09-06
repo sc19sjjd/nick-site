1a44140d50f5446c1b0aa3ab7c383477599c061a
Stuart Dingwell <stuartdingwell@gmail.com>
2024-09-06 11:59:27 +0100

    see more section add responsive layout

--- components/Index/Portoflio/SeeMore.vue
+++ components/Index/Portoflio/SeeMore.vue
@@ -26,7 +26,7 @@
         </div>
 
         <div class="relative z-10">
-          <img v-if="mainItemSource[0] === 'image'" class="w-full" :src="mainItemSource[1]" alt="test image" />
+          <img v-if="mainItemSource[0] === 'image'" class="w-full object-scale-down" :src="mainItemSource[1]" alt="test image" />
           <iframe v-else-if="mainItemSource[0] == 'video'" src="https://drive.google.com/file/d/1UZn6_WlV3CdlbrtT7JWvjVPVmNY9PYGR/preview" 
           width="640" height="360" allow="autoplay" allowfullscreen></iframe>
         </div>
@@ -37,32 +37,32 @@
 
   <WrappersContent>
     <div class="w-full flex flex-row justify-center">
-      <div class="bg-orange-100/5 z-20 p-3 flex flex-row justify-center items-center rounded-xl border-2 border-orange-500/60">
+      <div class="bg-orange-100/5 w-[95%] lg:w-auto object-contain z-20 p-3 flex gap-3 flex-col lg:flex-row justify-center 
+      items-center rounded-xl border-2 border-orange-500/60">
 
-        <div class="bar-none w-full max-w-52 h-96 overflow-y-auto flex flex-col items-center">
-          <div v-for="(src, index) in sources" :key="index" class="w-full pb-2 last:pb-0 z-20">
-            <div class="relative hover:cursor-pointer" @click="() => onClickPreview(index)">
-              <div class="absolute left-0 top-0 w-full h-full z-20 hover:border-4 opacity-0 bg-grey 
-              hover:border-orange-500 hover:opacity-50">
-              </div>
+        <div class="bar-none max-h-[27rem] max-w-4xl w-full lg:w-auto overflow-x-auto object-contain lg:overflow-y-auto gap-2 shrink-1 
+        lg:shrink-0 flex flex-row lg:flex-col items-center">
+          <div v-for="(src, index) in sources" :key="index" class="w-36 xs:w-44 sm:w-48 md:w-52 shrink-0 lg:pr-0 z-20 relative
+          hover:cursor-pointer" @click="() => onClickPreview(index)">
 
-              <img v-if="src[0] === 'image'" class="w-full h-full object-contain" :src="src[1]" alt="test image" />
-
-              <div v-else-if="src[0] == 'video'" class="w-full h-full relative">
-                <div class="absolute top-0 left-0 w-full h-full z-10 opacity-50 flex justify-center items-center">
-                  <img class="w-1/3" src="/public/icons/play-button-white.png" alt="play button" />
-                </div>
-                <img class="w-full h-full object-contain" :src="sources[0][1]" alt="test video preview" />
-              </div>
-
+            <div class="absolute left-0 top-0 w-full h-full z-20 hover:border-2 opacity-0 bg-grey 
+            hover:border-orange-500 hover:opacity-50">
             </div>
+
+            <img v-if="src[0] === 'image'" class="w-full h-full object-contain" :src="src[1]" alt="test image" />
+
+            <div v-else-if="src[0] == 'video'" class="w-full h-full relative">
+              <div class="absolute top-0 left-0 w-full h-full z-10 opacity-50 flex justify-center items-center">
+                <img class="w-1/3" src="/public/icons/play-button-white.png" alt="play button" />
+              </div>
+              <img class="w-full h-full object-contain" :src="sources[0][1]" alt="test video preview" />
+            </div>
+
           </div>
         </div>
 
-        <div class="pl-3"></div>
-
-        <div class="w-full max-w-2xl object-contain z-20 hover:cursor-pointer" @click="() => openSeeMoreModal()">
-          <img v-if="mainItemSource[0] === 'image'" class="w-full" :src="mainItemSource[1]" alt="test image" />
+        <div class="max-w-4xl object-contain z-20 hover:cursor-pointer relative" @click="() => openSeeMoreModal()">
+          <img v-if="mainItemSource[0] === 'image'" class="w-full object-scale-down" :src="mainItemSource[1]" alt="test image" />
           <iframe v-else-if="mainItemSource[0] == 'video'" src="https://drive.google.com/file/d/1UZn6_WlV3CdlbrtT7JWvjVPVmNY9PYGR/preview" 
           width="640" height="360" allow="autoplay" allowfullscreen></iframe>
         </div>
