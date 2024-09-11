6726589f18eae530b2f5e21fca9dd16659a017be
Stuart Dingwell <stuartdingwell@gmail.com>
2024-09-11 16:43:57 +0100

    simple carousel add arrow buttons and item selection

--- components/Index/Portoflio/SimpleCarousel.vue
+++ components/Index/Portoflio/SimpleCarousel.vue
@@ -1,16 +1,42 @@
 <template>
-  <div ref="outerDivEl" class="relative max-w-screen-2xl flex overflow-x-hidden">
-    <div ref="scrollContainerEl" class="flex list-none whitespace-nowrap overflow-x-auto snap-x snap-mandatory scroll-smooth shrink-0 w-full pb-2
-    gap-6">
-    <!-- :style="{ columnGap: `${scrollGap}px` }"> -->
-      <div class="pl-6 xs:hidden"></div>
+  <div class="relative w-full flex flex-col justify-center">
+    <div ref="outerDivEl" class="relative max-w-screen-2xl flex overflow-x-hidden">
+      <div ref="scrollContainerEl" class="flex list-none whitespace-nowrap overflow-x-auto snap-x snap-mandatory 
+      scroll-smooth shrink-0 w-full pb-2 gap-6">
+        <div class="pl-4 xs:hidden"></div>
 
-      <div v-for="(source, index) in props.data" :key="index" ref="listItemsEl" :id="`list-item${index}`" 
-      class="list-item shrink-0 snap-center xs:snap-start" :style="{ width: `${itemWidth}px` }">
-        <img ref="listItemImagesEl" class="rounded-md" :src="source.src" alt="Case study 1" />
+        <div v-for="(source, index) in props.data" :key="index" 
+        class="list-item shrink-0 snap-center xs:snap-start border-2 rounded-lg overflow-hidden hover:cursor-pointer" 
+        :style="{ width: `${itemWidth}px` }"
+        :class="{ 
+          'border-orange-500/70': currentSelectedItemIndex == index, 
+          'border-black/0 hover:border-lightGrey/60': currentSelectedItemIndex != index
+        }" 
+        @click="() => onClickItem(index)">
+          
+          <img ref="listItemImagesEl" :src="source.src" alt="Case study 1" />
+        
+        </div>
+
+        <div class="pl-4 xs:hidden"></div>
+      </div>
+    </div>
+
+    <div class="w-full flex justify-center gap-8 pt-4">
+
+      <div class="flex items-center justify-center w-12 h-12 rounded-full border-2 border-lightGrey/60 
+      hover:border-orange-500/70 hover:cursor-pointer transition duration-300 group" @click="() => onClickPreviousArrow()"
+      :class="{ 'disabled': currentLeftItemIndex == 0 }">
+        <img src="/public/icons/arrow-white.png" alt="arrow" class="w-8 h-8 opacity-50 group-hover:opacity-80 transition duration-300" 
+        style="transform: rotate(180deg);" />
       </div>
 
-      <div class="pl-6 xs:hidden"></div>
+      <div class="flex items-center justify-center w-12 h-12 rounded-full border-2 border-lightGrey/60 
+      hover:border-orange-500/70 hover:cursor-pointer transition duration-300 group" @click="() => onClickNextArrow()"
+      :class="{ 'disabled': currentLeftItemIndex == props.data.length - numItems }">
+        <img src="/public/icons/arrow-white.png" alt="arrow" class="w-8 h-8 opacity-50 group-hover:opacity-80 transition duration-300" />
+      </div>
+
     </div>
   </div>
 </template>
@@ -34,33 +60,11 @@
     background: #838383;
   }
 
-  /* .list-item {
-    width: 16rem;
+  .disabled {
+    opacity: 0.7;
+    cursor: initial;
+    pointer-events: none;
   }
-
-  @media (min-width: 480px) {
-    .list-item {
-      width: 13rem;
-    }
-  }
-
-  @media (min-width: 768px) {
-    .list-item {
-      width: 12rem;
-    }
-  }
-
-  @media (min-width: 1024px) {
-    .list-item {
-      width: 14rem;
-    }
-  }
-
-  @media (min-width: 1280px) {
-    .list-item {
-      width: 16rem;
-    }
-  } */
 </style>
 
 <script setup lang="ts">
@@ -70,37 +74,15 @@
     data: Array<PortfolioSource>
   }>();
 
-  const scrollContainerEl = ref<NullableHTMLElement>(null);
-  const listItemsEl = ref<Array<NullableHTMLElement>>([]);
   const outerDivEl = ref<NullableHTMLElement>(null);
+  const scrollContainerEl = ref<NullableHTMLElement>(null);
 
-  const scrollGap = ref(0);
   const itemWidth = ref(0);
   const numItems = ref(0);
-  
-  const updateScrollGap = () => {
-    if (!scrollContainerEl.value || !outerDivEl.value || !listItemsEl.value) {
-      return;
-    }
-
-    let outerDivWidth = outerDivEl.value!.clientWidth;
-    let listItemWidth = listItemsEl.value![0]!.clientWidth;
-
-    let numItems = Math.floor(outerDivWidth / listItemWidth);
-
-    console.log(outerDivWidth, listItemWidth, numItems);
-    
-    if (numItems > 1) {
-      scrollGap.value = (outerDivWidth - (numItems * listItemWidth)) / (numItems - 1);      
-    } else {
-      scrollGap.value = outerDivWidth - (numItems * listItemWidth);
-    }
-
-    console.log(scrollGap.value);
-  }
+  const itemGap = 24;
 
   const updateItemWidth = () => {
-    if (!scrollContainerEl.value || !outerDivEl.value) {
+    if (!outerDivEl.value) {
       return;
     }
 
@@ -108,17 +90,13 @@
 
     // minus 2 for visual adjustment
     if (numItems.value > 1) {
-      itemWidth.value = (outerDivWidth - ((numItems.value - 1) * 24)) / numItems.value - 2;
+      itemWidth.value = (outerDivWidth - ((numItems.value - 1) * itemGap)) / numItems.value - 2;
     } else {
-      itemWidth.value = outerDivWidth - 2;
+      itemWidth.value = outerDivWidth / 1.1;
     }
   }
 
   const updateNumItems = () => {
-    if (!scrollContainerEl.value || !outerDivEl.value) {
-      return;
-    }
-
     if (window.innerWidth < 480) {
       numItems.value = 1;
     } else if (window.innerWidth < 768 && window.innerWidth > 480) {
@@ -134,12 +112,43 @@
     updateNumItems();
     updateItemWidth();
   };
+
+  const currentSelectedItemIndex = defineModel('selectedItem', { type: Number, default: 0 });
+
+  const onClickItem = (index: number) => {
+    currentSelectedItemIndex.value = index;
+  }
+
+  const currentLeftItemIndex = ref(0);
+
+  const onScrollContainerScroll = (event: Event) => {
+    const scrollLeft = scrollContainerEl.value!.scrollLeft;
+    currentLeftItemIndex.value = Math.round(scrollLeft / itemWidth.value);
+  }
+
+  const onClickPreviousArrow = () => {
+    if (currentLeftItemIndex.value > 0) {
+      let left = currentLeftItemIndex.value * (itemWidth.value + itemGap) - (itemWidth.value + itemGap);
+      scrollContainerEl.value!.scrollTo({ left: left, behavior: 'smooth' });
+    }
+  }
+
+  const onClickNextArrow = () => {
+    if (currentLeftItemIndex.value < props.data.length - numItems.value) {
+      let left = currentLeftItemIndex.value * (itemWidth.value + itemGap) + (itemWidth.value + itemGap);
+      scrollContainerEl.value!.scrollTo({ left: left, behavior: 'smooth' });
+    }
+  }
   
   let resizeObserver: ResizeObserver;
 
   onMounted(() => {
-    updateScrollGap();
+    onOuterDivResize();
     resizeObserver = new ResizeObserver(onOuterDivResize);
     resizeObserver.observe(outerDivEl.value!);
+
+    if (scrollContainerEl.value) {
+      scrollContainerEl.value!.addEventListener('scroll', onScrollContainerScroll);
+    }
   });
 </script>
